2b3b47ff2f88d190e464e872c5c4cf44a9fb58f0
Sai-kumar <saikumarddd@gmail.com>
2025-05-30 22:38:50 +0530

    Updated Functions

--- vars/Functions.groovy
+++ vars/Functions.groovy
@@ -1,3 +1,16 @@
-def 
+def cdown(repo)
 {
-  
+    git 'https://github.com/sai-kadinti/${repo}'
+}
+
+def cbuild(mvn_stage)
+{
+    sh 'mvn ${mvn_stage}'
+}
+
+def cdeploy(job_name,ip,path)
+{
+    sh 'scp /var/lib/jenkins/workspace/${job_name}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${path}.war'
+}
+
+ 
