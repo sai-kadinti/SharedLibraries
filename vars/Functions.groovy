2a61dd202acbd5f8f37bf9b1728af27e46fffe6e
sai-kadinti <85397181+sai-kadinti@users.noreply.github.com>
2025-05-30 23:08:54 +0530

    Update Functions.groovy

--- vars/Functions.groovy
+++ vars/Functions.groovy
@@ -1,19 +1,19 @@
-def cdown(repo)
-{
-    git 'https://github.com/sai-kadinti/${repo}'
+def call() {
+    // Optional if you want to have a default step
 }
 
-def cbuild(mvn_stage)
-{
-    sh 'mvn ${mvn_stage}'
+def cdown(repo) {
+    git "https://github.com/sai-kadinti/${repo}"
 }
 
-def cdeploy(job_name,ip,path)
-{
-    sh 'scp /var/lib/jenkins/workspace/${job_name}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${path}.war'
+def cbuild(mvn_stage) {
+    sh "mvn ${mvn_stage}"
 }
 
-def ctest(jobname)
-{
-    sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
+def cdeploy(job_name, ip, path) {
+    sh "scp /var/lib/jenkins/workspace/${job_name}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${path}.war"
+}
+
+def ctest(jobname) {
+    sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
