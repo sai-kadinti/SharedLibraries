e3e26236502e1e103edb99fd75ab2882d86f5bb1
sai-kadinti <85397181+sai-kadinti@users.noreply.github.com>
2025-05-31 00:16:51 +0530

    Update ops.groovy

--- vars/ops.groovy
+++ vars/ops.groovy
@@ -3,7 +3,22 @@ def sum(a,b)
   echo "sum of ${a} and ${b} is ${a+b}"
 }
 
-def cdown(String repo)
-{
-  git 'https://github.com/sai-kadinti/${repo}.git'
+def call() {
+    // Optional if you want to have a default step
+}
+
+def cdown(repo) {
+    git "https://github.com/sai-kadinti/${repo}.git"
+}
+
+def cbuild(mvn_stage) {
+    sh "mvn ${mvn_stage}"
+}
+
+def cdeploy(job_name, ip, path) {
+    sh "scp /home/ubuntu/jenkins/workspace/${job_name}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${path}.war"
+}
+
+def ctest(jobname) {
+    sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
